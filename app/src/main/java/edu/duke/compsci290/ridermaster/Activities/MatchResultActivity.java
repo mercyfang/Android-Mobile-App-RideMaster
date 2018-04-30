7b31bb1248aa36a7c3e135943cb4db37d5f57907
Zhengzhi Xu <zhengzhixu@Zhengzhis-MacBook-Pro.local>
2018-04-30 15:46:04 -0400

    fixed minor bugs that prevent matchresult fromshowing

--- app/src/main/java/edu/duke/compsci290/ridermaster/Activities/MatchResultActivity.java
+++ app/src/main/java/edu/duke/compsci290/ridermaster/Activities/MatchResultActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -32,10 +33,11 @@ public class MatchResultActivity extends BaseNavDrawerActivity {
 
     ArrayList<LatLng> pickUpPointsLatLng = new ArrayList<>();
 
-    private Button mBackButton;
-    private Button mNewMatchButton;
-    private Button mNewTripButton;
+    private ConstraintLayout mBackButton;
+    private ConstraintLayout mNewMatchButton;
+    private ConstraintLayout mNewTripButton;
     private TextView mStatusText;
+
 
 
     @Override
