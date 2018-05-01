782088a283a9fd3603560af3d53ae082c27f7dbc
Jane <zx32@duke.edu>
2018-05-01 10:12:33 -0400

    waiting for mercy to do match so can do notification:

--- app/src/main/java/FirebaseDatabase/FirebaseDatabaseReaderWriter.java
+++ app/src/main/java/FirebaseDatabase/FirebaseDatabaseReaderWriter.java
@@ -128,20 +128,22 @@ public class FirebaseDatabaseReaderWriter {
 
 
     private void readUserEmailAndUpdateMatchResultActivity(String uId) {
-//        final DatabaseReference usersRef = root.child("users").child(uId);
-//        final String[] userEmail = new String[1];
-//        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                userEmail[0] = (String) dataSnapshot.child("email").getValue();
-//                MatchResultActivity.updateStatusTextView(userEmail[0]);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                Log.d(TAG, "Failed to read value.", databaseError.toException());
-//            }
-//        });
+        /*
+        final DatabaseReference usersRef = root.child("users").child(uId);
+        final String[] userEmail = new String[1];
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userEmail[0] = (String) dataSnapshot.child("email").getValue();
+                MatchResultActivity.updateStatusTextView(userEmail[0]);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+        */
     }
 
     public void deleteUserAndRideRequest(String uid, String requestId){
